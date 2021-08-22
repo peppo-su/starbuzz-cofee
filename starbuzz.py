3f86ed184912d72404a8aae49ee82e68c9ab4ff1
rafael <rafaellorcamar@gmail.com>
2021-08-22 15:52:55 -0500

    work with time module

--- starbuzz.py
+++ starbuzz.py
@@ -1,7 +1,9 @@
 import urllib.request
+import time
 price = 99.99
 
 while price > 4.74:
+    time.sleep(3)
     page = urllib.request.urlopen("file:///C:/Users/Hol/Videos/platzi/git/pj1/hfp/prices-loyalty.html")
     text = page.read().decode("utf8")
     buscar = text.find(">$")
