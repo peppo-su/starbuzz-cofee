79ea8eeb0d90f71ee7ce6fa088e5210ba2cdabb2
Yober Lopez <rafaellorcamar@gmail.com>
2021-08-22 15:39:20 -0500

    Show the price when is cheap

--- starbuzz.py
+++ starbuzz.py
@@ -1,7 +1,12 @@
 import urllib.request
-page = urllib.request.urlopen("file:///C:/Users/Hol/Videos/platzi/git/pj1/hfp/prices-loyalty.html")
-text = page.read().decode("utf8")
-buscar = text.find(">$")
-start = buscar + 2 
-end = start + 4
-print(text[start:end])
+price = 99.99
+
+while price > 4.74:
+    page = urllib.request.urlopen("file:///C:/Users/Hol/Videos/platzi/git/pj1/hfp/prices-loyalty.html")
+    text = page.read().decode("utf8")
+    buscar = text.find(">$")
+    start = buscar + 2 
+    end = start + 4
+    price = float(text[start:end])
+    print("!!!!!!!!!!!!")
+print("Buy!")
